05b26de156cb7a9f101df6198963a693bd74b08e
Rada Bogdan Raul <raoul_ice@yahoo.com>
2015-12-28 10:50:24 +0200

    don't change worker state to finished if exit status different than zero

--- lib/capistrano_multiconfig_parallel/celluloid/celluloid_worker.rb
+++ lib/capistrano_multiconfig_parallel/celluloid/celluloid_worker.rb
@@ -170,7 +170,7 @@ module CapistranoMulticonfigParallel
     def finish_worker(exit_status)
       log_to_file("worker #{job_id} tries to terminate with exit_status #{exit_status}")
       @manager.mark_completed_remaining_tasks(@job)
-      update_machine_state('FINISHED')
+      update_machine_state('FINISHED') if exit_status == 0
       @manager.workers_terminated.signal('completed') if @manager.alive? && @manager.all_workers_finished?
     end
 
