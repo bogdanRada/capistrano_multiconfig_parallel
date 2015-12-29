062c4bcdb9e9fbb36fa349fd63807fedc956f1b4
Rada Bogdan Raul <raoul_ice@yahoo.com>
2015-12-29 15:12:58 +0200

    update

--- lib/capistrano_multiconfig_parallel/version.rb
+++ lib/capistrano_multiconfig_parallel/version.rb
@@ -7,8 +7,8 @@ module CapistranoMulticonfigParallel
   # module used for generating the version
   module VERSION
     MAJOR = 0
-    MINOR = 25
-    TINY = 1
+    MINOR = 26
+    TINY = 0
     PRE = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
