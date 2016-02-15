5ec1ee70136e9e995fecf69f4da5e97132a88e92
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-02-15 10:59:17 +0200

    updated celluloid_pubsub

--- lib/capistrano_multiconfig_parallel/version.rb
+++ lib/capistrano_multiconfig_parallel/version.rb
@@ -8,7 +8,7 @@ module CapistranoMulticonfigParallel
   module VERSION
     MAJOR = 1
     MINOR = 1
-    TINY = 4
+    TINY = 5
     PRE = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
