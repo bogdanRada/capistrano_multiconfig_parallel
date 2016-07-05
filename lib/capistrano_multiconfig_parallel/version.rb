d3639cc50ad20f8f091777ae8f05460c3b841cd6
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-07-05 12:51:20 +0300

    relax rack dependency

--- lib/capistrano_multiconfig_parallel/version.rb
+++ lib/capistrano_multiconfig_parallel/version.rb
@@ -8,7 +8,7 @@ module CapistranoMulticonfigParallel
   module VERSION
     MAJOR = 1
     MINOR = 5
-    TINY = 1
+    TINY = 2
     PRE = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
