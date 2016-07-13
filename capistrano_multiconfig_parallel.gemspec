3eafb6a330696d412311fca338e4b4a9e8c13b98
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-07-13 12:21:50 +0300

    set logging for websockets

--- capistrano_multiconfig_parallel.gemspec
+++ capistrano_multiconfig_parallel.gemspec
@@ -29,7 +29,7 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency 'configliere', '~> 0.4', '>=0.4'
   s.add_runtime_dependency 'inquirer', '~> 0.2', '>= 0.2'
   s.add_runtime_dependency 'devnull','~> 0.1', '>= 0.1'
-  s.add_runtime_dependency 'capistrano_sentinel',  '>= 0.0', '>= 0.0.5'
+  s.add_runtime_dependency 'capistrano_sentinel',  '>= 0.0', '>= 0.0.9'
 
   s.add_development_dependency 'rake', '>= 10.4', '>= 10.4'
   s.add_development_dependency 'rspec', '~> 3.4', '>= 3.4'
--- lib/capistrano_multiconfig_parallel/classes/job_command.rb
+++ lib/capistrano_multiconfig_parallel/classes/job_command.rb
@@ -158,7 +158,7 @@ module CapistranoMulticonfigParallel
     def fetch_deploy_command
       #  config_flags = CapistranoMulticonfigParallel.configuration_flags.merge("capistrano_version": job_capistrano_version)
       environment_options = setup_command_line.join(' ')
-      command = "#{check_rvm_loaded} && if [ `which bundler |wc -l` = 0 ]; then gem install bundler;fi && (#{bundle_gemfile_env(job_gemfile_multi)} bundle check || #{bundle_gemfile_env(job_gemfile_multi)} bundle install ) && #{bundle_gemfile_env(job_gemfile_multi)} bundle exec cap #{job_stage} #{capistrano_action} #{environment_options}"
+      command = "#{check_rvm_loaded} && if [ `which bundler |wc -l` = 0 ]; then gem install bundler;fi && (#{bundle_gemfile_env(job_gemfile_multi)} bundle check || #{bundle_gemfile_env(job_gemfile_multi)} bundle install ) && WEBSOCKET_LOGGING=#{debug_websocket?} LOG_FILE=#{websocket_config.fetch('log_file_path', nil)} #{bundle_gemfile_env(job_gemfile_multi)} bundle exec cap #{job_stage} #{capistrano_action} #{environment_options}"
 
       command = "bash --login -c '#{command}'"  if rvm_enabled_for_job?
       command = command.inspect
