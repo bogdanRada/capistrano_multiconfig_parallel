fe385cdfdbaefe7701fe275acc6f7a7c6eecc264
rada bogdan raul <raoul_ice@yahoo.com>
2016-07-13 19:05:48 +0300

    Bump to beta1 version . Stable

--- lib/capistrano_multiconfig_parallel/version.rb
+++ lib/capistrano_multiconfig_parallel/version.rb
@@ -10,7 +10,7 @@ module CapistranoMulticonfigParallel
     MAJOR = 2
     MINOR = 0
     TINY = 0
-    PRE = 'beta'
+    PRE = 'beta1'
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
