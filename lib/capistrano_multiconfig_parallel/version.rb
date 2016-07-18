09be373ac950fa0da439b4aa27c7542e4def5949
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-07-18 14:15:39 +0300

    add configuration for checking app dependencies first

--- lib/capistrano_multiconfig_parallel/version.rb
+++ lib/capistrano_multiconfig_parallel/version.rb
@@ -10,7 +10,7 @@ module CapistranoMulticonfigParallel
     MAJOR = 2
     MINOR = 0
     TINY = 0
-    PRE = 'rc4'
+    PRE = 'rc5'
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
