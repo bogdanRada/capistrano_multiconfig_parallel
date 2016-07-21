5c3e2714e016f3f4b922108a71cf8255730585df
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-07-21 08:53:29 +0300

    activesupport 4.0

--- lib/capistrano_multiconfig_parallel/version.rb
+++ lib/capistrano_multiconfig_parallel/version.rb
@@ -8,8 +8,8 @@ module CapistranoMulticonfigParallel
   module VERSION
 
     MAJOR = 2
-    MINOR = 0
-    TINY = 9
+    MINOR = 1
+    TINY = 0
     PRE = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
