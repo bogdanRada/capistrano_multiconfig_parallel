a152ce996705c1d353c57eee6a323faac8107855
Rada Bogdan Raul <raoul_ice@yahoo.com>
2017-05-23 10:56:07 +0300

    fix terminal width wrapping

--- lib/capistrano_multiconfig_parallel/version.rb
+++ lib/capistrano_multiconfig_parallel/version.rb
@@ -8,8 +8,8 @@ module CapistranoMulticonfigParallel
   module VERSION
 
     MAJOR = 2
-    MINOR = 6
-    TINY = 2
+    MINOR = 7
+    TINY = 0
     PRE = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
